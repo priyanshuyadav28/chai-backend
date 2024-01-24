d54945b672cc0211e42f25c6dcc41be032bb57c6
priyanshu Yadav <priyanshuyadav28027@gmail.com>
2024-01-24 17:15:43 +0530

    fixed issue in import statements in auth middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,31 @@ import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/apiResponse.js"
 
+
+// method to generate access token 
+const generateAccessAndRefreshTokens = async (userId) => {
+    try {
+        // find the user to generate token 
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        // insert refresh token in user object 
+        user.refreshToken = refreshToken
+        // save the refresh token in db model
+        await user.save({ validateBeforeSave: false })
+
+        return {accessToken, refreshToken}
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating refresh and access token")
+    }
+}
+
+
+
+
+// register/signup user 
 const registerUser = asyncHandler( async (req, res) => {
     // get user details from frontend
     // validation - not empty 
@@ -16,8 +41,6 @@ const registerUser = asyncHandler( async (req, res) => {
     // return res
 
     const {fullName, email, username, password } = req.body
-    console.log("email: ", email);
-    // console.log("req.body: ", req.body);
 
     // if (fullName === "") {
     //     throw new ApiError(400, "fullname is required")
@@ -96,4 +119,95 @@ const registerUser = asyncHandler( async (req, res) => {
 
 
 
-export {registerUser}
+
+
+// login user 
+
+const loginUser = asyncHandler( async (req, res) => {
+    // req body -> data 
+    // username, password exist or not 
+    // check user exist or not 
+    // if exist compare the password -> password check 
+    // access and refresh token 
+    // send cookies 
+    
+    const {email, username, password} = req.body
+
+    if (!(username || email)) {
+        throw new ApiError(400, "username or email is required")
+    }
+
+    const user = await User.findOne({
+        $or: [{username}, {email}],
+    })
+
+    if (!user) {
+        throw new ApiError(404, "User does not exist")
+    }
+
+    // now when user found password check
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid user credentials")
+    }
+
+    // generate access and refresh token 
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+    
+    // send cookie 
+    const options = {
+        httpOnly: true, 
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200, 
+            {
+                user: loggedInUser, accessToken, refreshToken
+            }, 
+            "User logged In successfully "
+        )
+    )
+
+})
+
+
+
+// logout user 
+const logoutUser = asyncHandler(async (req, res) => {
+    
+    await User.findByIdAndUpdate(
+        req.user._id, 
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        }, 
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+
+    return res
+      .status(200)
+      .clearCookie("accessToken", options)
+      .clearCookie("refreshToken", options)
+      .json(new ApiResponse(200, {}, "User logged Out"))
+
+})
+
+
+export { registerUser, loginUser, logoutUser };
--- src/db/database.js
+++ src/db/database.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
-    console.log(`\n MongoDB connected !! DB HOST: 
+    console.log(`\nMongoDB connected !! DB HOST: 
         ${connectionInstance.connection.host}`);
     // console.log(connectionInstance);
   } catch (error) {
